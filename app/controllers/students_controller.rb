8a685207d27390597b4a7ca8f970e8204c1cbf4e
takumanaruke <leon32taku@gmail.com>
2018-08-21 17:17:02 +0900

    modified routes and student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,22 +1,46 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
 
   def show
+    find_student
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new
   end
 
   def edit
+    find_student
   end
 
   def update
+    find_student
+    @student = Student.update(student_params)
+    if @student.save
+      redirect_to student_show_path(@student)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @student.destroy
+    redirect_to students_path
+  end
+
+  private
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
+  def lesson_params
+    params.require(:student).permit(:name, :birth_date, :category, :description)
   end
 end
