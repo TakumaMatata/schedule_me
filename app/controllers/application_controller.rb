23dc85f3641aa6d3f6c4fcfb38063052b6aff92a
Fumiko Toyoda <toyodafumiko@gmail.com>
2018-08-21 16:10:47 +0900

    added simple calendar gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+    private
+
+    def set_time_zone
+      Time.zone = current_user.time_zone
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module ScheduleMe
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.beginning_of_week = :monday
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
