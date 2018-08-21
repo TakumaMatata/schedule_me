b51406daa638225d085c4348e0ec9af1f1f46647
Caleb Finley <calebfinley989@gmail.com>
2018-08-21 14:44:01 +0900

    Added User, Lesson, and Student Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'students/index'
+  get 'students/show'
+  get 'students/new'
+  get 'students/create'
+  get 'students/edit'
+  get 'students/update'
+  get 'students/destroy'
+  get 'lessons/index'
+  get 'lessons/show'
+  get 'lessons/new'
+  get 'lessons/create'
+  get 'lessons/edit'
+  get 'lessons/update'
+  get 'lessons/destroy'
+  get 'users/index'
+  get 'users/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
