668fa6f7f856f2784cdb3ec442926c5f849eefca
Fumiko Toyoda <toyodafumiko@gmail.com>
2018-08-21 15:18:32 +0900

    created seed file

--- db/migrate/20180821055001_remove_duration_from_lesson.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDurationFromLesson < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :lessons, :duration, :integer
-  end
-end
--- db/migrate/20180821055017_add_duration_to_course.rb
+++
@@ -1,5 +0,0 @@
-class AddDurationToCourse < ActiveRecord::Migration[5.2]
-  def change
-    add_column :courses, :duration, :integer
-  end
-end
