9a9626a0cda7032d31660d77ae7d2018bb78a6ad
Caleb Finley <calebfinley989@gmail.com>
2018-08-22 11:01:01 +0900

    Added all validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ApplicationRecord
   belongs_to :term
   belongs_to :user
   has_many :lessons
+  validates :language, presence: true, inclusion: {in: ["English"]}
+  validates :level, presence: true, inclusion: {in: ["Novice", "Conversational", "Business", "Advanced"]}
+  validates :duration, presence: true, inclusion: {in: [30, 45, 60, 90]}
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,4 @@
 class Room < ApplicationRecord
   has_many :lessons
+  validates :name, presence: true
 end
