22d707718fb3ad7c32411d018b98bce1536e5d21
Nozomi Ishii <non.ankt@gmail.com>
2018-08-22 14:15:58 +0900

    build path from profile button on navbar to the page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :students
-  resources :user, only: [:index, :show] do
+  resources :users, only: [:index, :show] do
     resources :lessons
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
