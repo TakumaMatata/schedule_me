245418df82de83dfa17dad9895594b2685a7a2ca
Nozomi Ishii <non.ankt@gmail.com>
2018-08-22 20:56:42 +0900

    update seed file to add photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,15 @@ Term.delete_all
 
 puts "Deleted all the past data"
 
+test_url = "https://kitt.lewagon.com/placeholder/users/takumamatata"
 test_user= User.new(email: "test@gmail.com", password: "123456", first_name: "Takuma", last_name: "Naruke", role: "manager")
+test_user.remote_photo_url = test_url
 test_user.save
 
 10.times do
+  url = "https://kitt.lewagon.com/placeholder/users/random"
   user_new = User.new(email: Faker::Internet.email, password: "123456", first_name: Faker::HarryPotter.character.split[0], last_name: Faker::HarryPotter.character.split[1], role: "teacher")
+  user_new.remote_photo_url = url
   user_new.save
 end
 
