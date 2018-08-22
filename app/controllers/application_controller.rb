7ab78be0045787d03a83f446a58717c396fa0acb
takumanaruke <leon32taku@gmail.com>
2018-08-22 14:25:07 +0900

    created lesson new page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
 
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
 
     private
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
       Time.zone = current_user.time_zone
     end
 
-    def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-  end
+  #   def skip_pundit?
+  #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  # end
 end
