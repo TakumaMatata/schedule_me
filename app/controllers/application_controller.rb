cc905e72a08ab39e8468faa86ec858c5cd4f3e86
Caleb Finley <calebfinley989@gmail.com>
2018-08-22 15:10:56 +0900

    linked login to lesson index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,8 @@ class ApplicationController < ActionController::Base
   #   def skip_pundit?
   #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   # end
+
+  def after_sign_in_path_for(resource)
+    lessons_path
+  end
 end
