25dc45dbacffc8a2494e78c6ea274ebf10d99c41
Caleb Finley <calebfinley989@gmail.com>
2018-08-24 12:13:13 +0900

    Added a students option to Create Lesson form

--- app/javascript/plugins/multiselect.js
+++
@@ -1,4 +0,0 @@
-    $(document).ready(function() {
-        $('#example-getting-started').multiselect();
-    });
-
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,10 @@ class Lesson < ApplicationRecord
   has_many :enrollments
   has_many :students, through: :enrollments
   validates :start_time, presence: true
+
+  def add_students(student_ids)
+    student_ids.each do |id|
+      Enrollment.create(lesson: self, student_id: id)
+    end
+  end
 end
