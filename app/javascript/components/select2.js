224705678d20e4806f9c64d517b02a631c414c45
Caleb Finley <calebfinley989@gmail.com>
2018-08-28 18:24:02 +0900

    beginning redesign of lesson show

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'select2';
 
-$('.select2').select2();
+$('#students .select2').select2();
+$('#teachers-select2 .select2').select2();
 
 import 'select2/dist/css/select2.css';
